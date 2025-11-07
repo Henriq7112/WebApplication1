c05e5e666a78a0d34e5eef5a5ac244ffb106fb55
Henriq7112 <hsdas7112@gmail.com>
2025-11-07 16:33:41 -0300

    Configuração do banco de dados e da Tabela Veículo

---
+++ Models/AppDbConetx.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Models
+{
+    public class AppDbConetx : DbContext
+    {
+        public AppDbConetx(DbContextOptions<AppDbConetx> options) : base(options)  { }
+
+        public DbSet<Veiculo> Veiculos { get; set; }
+    }
+}
