6252da2dba947bd2f24e234a8fe95a62d817387c
Henriq7112 <hsdas7112@gmail.com>
2025-11-11 11:48:22 -0300

    Criação de relatorio

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace WebApplication1.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            return RedirectToAction("Index", "Veiculos");
         }
 
         public IActionResult Privacy()
